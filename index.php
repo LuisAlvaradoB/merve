1e9e8bd3f6bbdc8c8adc6536a2a8b19f4c1527b4
Luis Alvarado <luis.alvaradofcb@outlook.es>
2019-05-24 13:59:26 -0400

    Se suben los iconos del nav

--- index.php
+++ index.php
@@ -1,226 +1,232 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<title>Casa Molle Villa & Golf | Un hotel único en el mundo</title>
-	<?php require 'assets/partials/header.php'?>
-	
-	<body>
-		<div class="preloader"></div>
-		<header class="header transp sticky">
-			<nav class="navbar navbar-inverse">
-				<div class="container">
-					<div class="navbar navbar-expand-md justify-content-between"><a class="navbar-brand" href="index.php"><img src="assets/images/Logo_Casa_molle.png" alt="logo"></a>
-						<div class="mg-navs">
-							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-							<div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
-								<ul class="navbar-nav navbar-right">
-									<li class="active"><a href="index.php">HOME</a>
-									</li>
-                  			<li class="dropdown"><a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">EXPERIENCIAS</a>
-										<ul class="dropdown-menu">
-											<li><a href="great-moment.php">Great Moment</a>
-                      				</li>
-                      				<li><a href="big-family.php">Big Family</a>
-                      				</li>
-                      				<li><a href="business-meetings.php">Business & Meetings</a>
-                      				</li>
-                      				<li><a href="weddings-events.php">Weddings & Events</a>
-                      				</li>
-                    				</ul>
-                  			</li>	
-									<li><a href="galeria.php">GALERIA</a>
-									</li>
-									<li><a href="#testimonial-nav">COMUNIDAD</a>
-									</li>
-									<li><a href="contacto.php">CONTACTO</a>
-									</li>
-								</ul>
-							</div>
+<title>Casa Molle Villa & Golf | Un hotel único en el mundo</title>
+<?php require 'assets/partials/header.php' ?>
+
+<body>
+	<div class="preloader"></div>
+	<header class="header transp sticky">
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar navbar-expand-md justify-content-center"><a class="navbar-brand" href="index.php">
+					<i class="fas fa-grip-lines"></i>
+					<img src="assets/images/Logo_Casa_molle.png" alt="logo"></a>
+					<i class="far fa-calendar-alt"></i>
+					<div class="mg-navs">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+						<div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+							<ul class="navbar-nav navbar-right">
 							
+								<!-- <li class="active"><a href="index.php">HOME</a>
+								</li>
+								<li class="dropdown"><a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">EXPERIENCIAS</a>
+									<ul class="dropdown-menu">
+										<li><a href="great-moment.php">Great Moment</a>
+										</li>
+										<li><a href="big-family.php">Big Family</a>
+										</li>
+										<li><a href="business-meetings.php">Business & Meetings</a>
+										</li>
+										<li><a href="weddings-events.php">Weddings & Events</a>
+										</li>
+									</ul>
+								</li>
+								<li><a href="galeria.php">GALERIA</a>
+								</li>
+								<li><a href="#testimonial-nav">COMUNIDAD</a>
+								</li>
+								<li><a href="contacto.php">CONTACTO</a>
+								</li> -->
+							</ul>
 						</div>
+
 					</div>
 				</div>
-			</nav>
-		</header>
+			</div>
+		</nav>
+	</header>
 
-		<?php require 'assets/partials/carousel-slide.php'?>
-		<?php require 'assets/partials/reserva-section.php'?>
+	<?php require 'assets/partials/carousel-slide.php' ?>
+	<?php require 'assets/partials/reserva-section.php' ?>
 
-		<section id="main-section-experiencias" class="mg-best-rooms">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="mg-sec-title undefined">
-							<h2>EXPLORA CASA MOLLE</h2>
+	<section id="main-section-experiencias" class="mg-best-rooms">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="mg-sec-title undefined">
+						<h2>EXPLORA CASA MOLLE</h2>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<figure class="mg-room "><img class="img-fluid" src="assets/images/GREAT_MOMENT1.png" alt="img-greatMoment" />
+								<figcaption style="top:200px">
+									<h2>GREAT MOMENT</h2>
+									<div class="mg-room-rating"><i class="fa fa-star"> </i>
+									</div>
+
+									<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="great-moment.php">Descubre</a>
+								</figcaption>
+							</figure>
 						</div>
-						<div class="row">
-							<div class="col-md-3">
-								<figure class="mg-room "><img class="img-fluid" src="assets/images/GREAT_MOMENT1.png" alt="img-greatMoment"/>
-									<figcaption style="top:200px">
-										<h2>GREAT MOMENT</h2>
-										<div class="mg-room-rating"><i class="fa fa-star"> </i> 
-										</div>
+						<div class="col-md-3">
+							<figure class="mg-room "><img class="img-fluid" src="assets/images/BIG_FAMILY.png" alt="img-bigFamily" />
+								<figcaption style="top:200px">
+									<h2>BIG FAMILY</h2>
+									<div class="mg-room-rating"><i class="fa fa-star"> </i>
+									</div>
 
-										<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="great-moment.php">Descubre</a>
-									</figcaption>
-								</figure>
-							</div>
-							<div class="col-md-3">
-								<figure class="mg-room "><img class="img-fluid" src="assets/images/BIG_FAMILY.png" alt="img-bigFamily"/>
-									<figcaption style="top:200px">
-										<h2>BIG FAMILY</h2>
-										<div class="mg-room-rating"><i class="fa fa-star"> </i>
-										</div>
-										
-										<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="big-family.php">Descubre</a>
-									</figcaption>
-								</figure>
-							</div>
-							<div class="col-md-3">
-								<figure class="mg-room "><img class="img-fluid" src="assets/images/BUSINESS_MEETINGS.png" alt="img-business"/>
-									<figcaption style="top:200px">
-										<h2>BUSINESS & MEETINGS</h2>
-										<div class="mg-room-rating"><i class="fa fa-star"> </i> 
-										</div>
-										
-										<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="business-meetings.php">Descubre</a>
-									</figcaption>
-								</figure>
-							</div>
-							<div class="col-md-3">
-								<figure class="mg-room "><img class="img-fluid" src="assets/images/WEDDINGS_EVENTS_.png" alt="img-weddings"/>
-									<figcaption style="top:200px">
-										<h2>WEDDINGS & EVENTS</h2>
-										<div class="mg-room-rating"><i class="fa fa-star"> </i>
-		
-										</div>
-										
-										<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="weedings-events.php">Descubre</a>
-									</figcaption>
-								</figure>
-							</div>
+									<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="big-family.php">Descubre</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-3">
+							<figure class="mg-room "><img class="img-fluid" src="assets/images/BUSINESS_MEETINGS.png" alt="img-business" />
+								<figcaption style="top:200px">
+									<h2>BUSINESS & MEETINGS</h2>
+									<div class="mg-room-rating"><i class="fa fa-star"> </i>
+									</div>
+
+									<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="business-meetings.php">Descubre</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-3">
+							<figure class="mg-room "><img class="img-fluid" src="assets/images/WEDDINGS_EVENTS_.png" alt="img-weddings" />
+								<figcaption style="top:200px">
+									<h2>WEDDINGS & EVENTS</h2>
+									<div class="mg-room-rating"><i class="fa fa-star"> </i>
+
+									</div>
+
+									<p>adversantur probatum iudicante indicaverunt repugnantibus.</p><a class="btn btn-main" href="weedings-events.php">Descubre</a>
+								</figcaption>
+							</figure>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section>
-		
+		</div>
+	</section>
 
-		<section class="mg-book-now">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-4 col-xs-12">
-						<div class="row">
-							<h3 style="color: #e7b315">Conecta con CasaMolle</h3>
-						</div>	
+
+	<section class="mg-book-now">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-xs-12">
+					<div class="row">
+						<h3 style="color: #e7b315">Conecta con CasaMolle</h3>
 					</div>
-					<div class="col-lg-6 col-xs-12">
-						<div class="row">
-							<a href="https://www.instagram.com/casamollevillaandgolf/" target="_blanck"><i class=" fab fa-instagram fa-3x" style="color:white; padding-right: 30px;"></i></a>
-							<a href="https://www.facebook.com/CasaMolleVilla/" target="_blank"><i class=" fab fa-facebook fa-3x" style="color:white; padding-right: 30px;"></i></a>
-							<a href="https://twitter.com/casamollevilla" target="_blanck"><i class=" fab fa-twitter fa-3x" style="color:white; padding-right: 30px;"></i></a>
-							<a href="https://cl.linkedin.com/company/casamollevilla" target="_blanck"><i class=" fab fa-linkedin fa-3x" style="color:white; padding-right: 30px;"></i>
+				</div>
+				<div class="col-lg-6 col-xs-12">
+					<div class="row">
+						<a href="https://www.instagram.com/casamollevillaandgolf/" target="_blanck"><i class=" fab fa-instagram fa-3x" style="color:white; padding-right: 30px;"></i></a>
+						<a href="https://www.facebook.com/CasaMolleVilla/" target="_blank"><i class=" fab fa-facebook fa-3x" style="color:white; padding-right: 30px;"></i></a>
+						<a href="https://twitter.com/casamollevilla" target="_blanck"><i class=" fab fa-twitter fa-3x" style="color:white; padding-right: 30px;"></i></a>
+						<a href="https://cl.linkedin.com/company/casamollevilla" target="_blanck"><i class=" fab fa-linkedin fa-3x" style="color:white; padding-right: 30px;"></i>
 							<a href="https://www.tripadvisor.cl/Hotel_Review-g970257-d15289729-Reviews-Casamolle_Villa_Golf-Vicuna_Coquimbo_Region.html" target="_blanck"><i class=" fab fa-tripadvisor fa-3x" style="color:white; padding-right: 30px;"></i></a>
-						</div>
 					</div>
-					<div class="col-lg-2 col-xs-12">
-						<div class="row">
-							<a href="https://casamolle.paxer.com/reservacion" target="_blank" class="btn btn-main btn-block ">Descargar Brochure</a>
-						</div>
-					</div> <!-- col-lg-9 -->
 				</div>
+				<div class="col-lg-2 col-xs-12">
+					<div class="row">
+						<a href="https://casamolle.paxer.com/reservacion" target="_blank" class="btn btn-main btn-block ">Descargar Brochure</a>
+					</div>
+				</div> <!-- col-lg-9 -->
 			</div>
-		</section>
-		<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/b94c1f1862f9503aa09650224b4852ee.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+		</div>
+	</section>
+	<!-- LightWidget WIDGET -->
+	<script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/b94c1f1862f9503aa09650224b4852ee.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
 
-		
 
 
 
-		<section class="mg-features">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="mg-sec-title undefined">
-							<h2>Comodidades del Hotel</h2>
-							<p>These best rooms chosen by our customers</p>
-						</div>
-						<div class="row">
-							<div class="col-md-4">
-								<div class="mg-feature">
-									<div class="mg-feature-icon-title"><i class="fp-ht-receptionist"></i>
-										<h3>24-hour reception</h3>
-									</div>
-									<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+
+	<section class="mg-features">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="mg-sec-title undefined">
+						<h2>Comodidades del Hotel</h2>
+						<p>These best rooms chosen by our customers</p>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+							<div class="mg-feature">
+								<div class="mg-feature-icon-title"><i class="fp-ht-receptionist"></i>
+									<h3>24-hour reception</h3>
 								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="mg-feature">
-									<div class="mg-feature-icon-title"><i class="fa fa-cogs"></i>
-										<h3>Room service</h3>
-									</div>
-									<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="mg-feature">
-									<div class="mg-feature-icon-title"><i class="fa fa-car"></i>
-										<h3>Car hire</h3>
-									</div>
-									<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
-								</div>
+								<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
 							</div>
 						</div>
-						<div class="row">
-							<div class="col-md-4">
-								<div class="mg-feature">
-									<div class="mg-feature-icon-title"><i class="fa fa-phone"></i>
-										<h3>Wake-up call</h3>
-									</div>
-									<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+						<div class="col-md-4">
+							<div class="mg-feature">
+								<div class="mg-feature-icon-title"><i class="fa fa-cogs"></i>
+									<h3>Room service</h3>
 								</div>
+								<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
 							</div>
-							<div class="col-md-4">
-								<div class="mg-feature">
-									<div class="mg-feature-icon-title"><i class="fa fa-coffee"></i>
-										<h3>Coffee and tea</h3>
-									</div>
-									<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+						</div>
+						<div class="col-md-4">
+							<div class="mg-feature">
+								<div class="mg-feature-icon-title"><i class="fa fa-car"></i>
+									<h3>Car hire</h3>
 								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="mg-feature">
-									<div class="mg-feature-icon-title"><i class="fp-ht-computer"></i>
-										<h3>Free Wi-Fi</h3>
-									</div>
-									<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
-								</div>
+								<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
 							</div>
 						</div>
 					</div>
+					<div class="row">
+						<div class="col-md-4">
+							<div class="mg-feature">
+								<div class="mg-feature-icon-title"><i class="fa fa-phone"></i>
+									<h3>Wake-up call</h3>
+								</div>
+								<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="mg-feature">
+								<div class="mg-feature-icon-title"><i class="fa fa-coffee"></i>
+									<h3>Coffee and tea</h3>
+								</div>
+								<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="mg-feature">
+								<div class="mg-feature-icon-title"><i class="fp-ht-computer"></i>
+									<h3>Free Wi-Fi</h3>
+								</div>
+								<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 
-		<?php require 'assets/partials/testimonial-slider.php'?>
-		<?php require 'assets/partials/footer.php'?>
-		<!--
+	<?php require 'assets/partials/testimonial-slider.php' ?>
+	<?php require 'assets/partials/footer.php' ?>
+	<!--
 		All JavaScripts Codes Loaded
 		Ex: jquery, bootstrap, etc.
 		-->
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
-		<script src="assets/js/owl.carousel.min.js"></script>
-		<script src="assets/js/jssor.slider.mini.js"></script>
-		<script src="assets/js/classie.js"></script>
-		<script src="assets/js/selectFx.js"></script>
-		<script src="assets/js/bootstrap-datepicker.min.js"></script>
-		<script src="assets/js/starrr.min.js"></script>
-		<script src="assets/js/nivo-lightbox.min.js"></script>
-		<script src="assets/js/jquery.shuffle.min.js"></script>
-		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-		<script src="assets/js/gmaps.min.js"></script>
-		<script src="assets/js/jquery.parallax-1.1.3.js"></script>
-		<script src="assets/js/scripts.js"></script>
-	</body>
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jssor.slider.mini.js"></script>
+	<script src="assets/js/classie.js"></script>
+	<script src="assets/js/selectFx.js"></script>
+	<script src="assets/js/bootstrap-datepicker.min.js"></script>
+	<script src="assets/js/starrr.min.js"></script>
+	<script src="assets/js/nivo-lightbox.min.js"></script>
+	<script src="assets/js/jquery.shuffle.min.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="assets/js/gmaps.min.js"></script>
+	<script src="assets/js/jquery.parallax-1.1.3.js"></script>
+	<script src="assets/js/scripts.js"></script>
+</body>
+
 </html>
