f788c55121457a752104ecf2601f30bee83fd018
Luis Alvarado <luis.alvaradofcb@outlook.es>
2019-05-25 10:45:12 -0400

    Formato al documento

--- big-family.php
+++ big-family.php
@@ -1,175 +1,176 @@
 <!DOCTYPE html>
 <html lang="en">
-	
-  <title>Big Family | Casa Molle Villa & Golf</title>
-  <?php require 'assets/partials/header.php'?>
 
-	<body>
-		<div class="preloader"></div>
-		<header class="header transp sticky">
-			<nav class="navbar navbar-inverse">
-				<div class="container">
-					<div class="navbar navbar-expand-md justify-content-between"><a class="navbar-brand" href="index.php"><img src="assets/images/Logo_Casa_molle.png" alt="logo"></a>
-						<div class="mg-navs">
-							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-							<div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
-								<ul class="navbar-nav navbar-right">
-                  			<li><a href="index.php">HOME</a>
-									</li>
-                  			<li class="dropdown active"><a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">EXPERIENCIAS</a>
-										<ul class="dropdown-menu">
-											<li><a href="great-moment.php">Great Moment</a>
-                      				</li>
-                      				<li><a href="big-family.php">Big Family</a>
-                      				</li>
-                      				<li><a href="business-meetings.php">Business & Meetings</a>
-                      				</li>
-                      				<li><a href="weddings-events.php">Weddings & Events</a>
-                      				</li>
-                    				</ul>
-                  			</li>	
-									<li><a href="galeria.php">GALERIA</a>
-									</li>
-									<li><a href="index.php#testimonial-nav">COMUNIDAD</a>
-									</li>
-									<li><a href="contacto.php">CONTACTO</a>
-									</li>
-								</ul>
-							</div>
+<title>Big Family | Casa Molle Villa & Golf</title>
+<?php require 'assets/partials/header.php' ?>
+
+<body>
+	<div class="preloader"></div>
+	<header class="header transp sticky">
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar navbar-expand-md justify-content-between"><a class="navbar-brand" href="index.php"><img src="assets/images/Logo_Casa_molle.png" alt="logo"></a>
+					<div class="mg-navs">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+						<div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+							<ul class="navbar-nav navbar-right">
+								<li><a href="index.php">HOME</a>
+								</li>
+								<li class="dropdown active"><a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">EXPERIENCIAS</a>
+									<ul class="dropdown-menu">
+										<li><a href="great-moment.php">Great Moment</a>
+										</li>
+										<li><a href="big-family.php">Big Family</a>
+										</li>
+										<li><a href="business-meetings.php">Business & Meetings</a>
+										</li>
+										<li><a href="weddings-events.php">Weddings & Events</a>
+										</li>
+									</ul>
+								</li>
+								<li><a href="galeria.php">GALERIA</a>
+								</li>
+								<li><a href="index.php#testimonial-nav">COMUNIDAD</a>
+								</li>
+								<li><a href="contacto.php">CONTACTO</a>
+								</li>
+							</ul>
 						</div>
 					</div>
 				</div>
-			</nav>
-		</header>
-		<div class="mg-page-title main-img-big-family parallax">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h2>Big Family</h2>
+			</div>
+		</nav>
+	</header>
+	<div class="mg-page-title main-img-big-family parallax">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Big Family</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="mg-about-features">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>Inolvidable experiencia para tu familia. Aquí se comparten momentos memorables de entretención, conversación y actividades familiares. Organizamos grupos para generar entretenidas dinámicas de juegos de salón, deportivas, karaoke, golf y mucho más. Nietos con abuelos, padres con hijos o sobrinos, mamá e hijas en una experiencia muy enriquecedora.Una gran familia tiene grandes personajes, ven y descubre lo mejor de cada uno de ellos. <br> Consulte por las cortesías asociadas a este programa.</p>
+					<hr>
+				</div>
+				<div class="col-sm-4">
+					<div class="mg-feature">
+						<div class="mg-feature-icon-title"><i class="fp-ht-maid"></i>
+							<h3>Room service</h3>
+						</div>
+						<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="mg-feature">
+						<div class="mg-feature-icon-title"><i class="fp-ht-computer"></i>
+							<h3>Wi-fi service</h3>
+						</div>
+						<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="mg-feature">
+						<div class="mg-feature-icon-title"><i class="fp-ht-parking"></i>
+							<h3>Free Parking</h3>
+						</div>
+						<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="mg-about-features">
+
+		<section class="mg-news-gallery">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12">
-						<p>Inolvidable experiencia para tu familia. Aquí se comparten momentos memorables de entretención, conversación y actividades familiares. Organizamos grupos para generar entretenidas dinámicas de juegos de salón, deportivas, karaoke, golf y mucho más. Nietos con abuelos, padres con hijos o sobrinos, mamá e hijas en una experiencia muy enriquecedora.Una gran familia tiene grandes personajes, ven y descubre lo mejor de cada uno de ellos. <br> Consulte por las cortesías asociadas a este programa.</p>
-						<hr>
-					</div>
-					<div class="col-sm-4">
-						<div class="mg-feature">
-							<div class="mg-feature-icon-title"><i class="fp-ht-maid"></i>
-								<h3>Room service</h3>
-							</div>
-							<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="mg-feature">
-							<div class="mg-feature-icon-title"><i class="fp-ht-computer"></i>
-								<h3>Wi-fi service</h3>
-							</div>
-							<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="mg-feature">
-							<div class="mg-feature-icon-title"><i class="fp-ht-parking"></i>
-								<h3>Free Parking</h3>
-							</div>
-							<p>Didicisset labore vitium referenda labor peccant integre turpe est tantopere, eius defuturum sua dolorum.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<section class="mg-news-gallery">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-2"></div>
-						<div class="col-md-8">
-							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-								<ol class="carousel-indicators">
-									<li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
-									<li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
-								</ol>
-								<div class="carousel-inner">
-									<div class="carousel-item active">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (1).png" alt="img1">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (2).png" alt="img2">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (3).png" alt="img3">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (4).png" alt="img4">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (5).png" alt="img5">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (6).png" alt="img6">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (7).png" alt="img7">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (8).png" alt="img8">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (9).png" alt="img9">
-									</div>
-									<div class="carousel-item">
-										<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (10).png" alt="img10">
-									</div>
-
-									<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-										<span class="sr-only">Previous</span>
-									</a>
-									<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-										<span class="carousel-control-next-icon" aria-hidden="true"></span>
-										<span class="sr-only">Next</span>
-									</a>
+					<div class="col-md-2"></div>
+					<div class="col-md-8">
+						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							<ol class="carousel-indicators">
+								<li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+							</ol>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (1).png" alt="img1">
 								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (2).png" alt="img2">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (3).png" alt="img3">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (4).png" alt="img4">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (5).png" alt="img5">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (6).png" alt="img6">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (7).png" alt="img7">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (8).png" alt="img8">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (9).png" alt="img9">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="assets/images/galeria-interna/big-family/Big_Family (10).png" alt="img10">
+								</div>
+
+								<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
 							</div>
 						</div>
-						<div class="col-md-2"></div>
 					</div>
-				</div>	
-			</section> 
-		</div>
-		
-		
-		<?php require 'assets/partials/footer.php'?>
-		<!--
+					<div class="col-md-2"></div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+
+	<?php require 'assets/partials/footer.php' ?>
+	<!--
 		All JavaScripts Codes Loaded
 		Ex: jquery, bootstrap, etc.
 		-->
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
-		<script src="assets/js/owl.carousel.min.js"></script>
-		<script src="assets/js/jssor.slider.mini.js"></script>
-		<script src="assets/js/classie.js"></script>
-		<script src="assets/js/selectFx.js"></script>
-		<script src="assets/js/bootstrap-datepicker.min.js"></script>
-		<script src="assets/js/starrr.min.js"></script>
-		<script src="assets/js/nivo-lightbox.min.js"></script>
-		<script src="assets/js/jquery.shuffle.min.js"></script>
-		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-		<script src="assets/js/gmaps.min.js"></script>
-		<script src="assets/js/jquery.parallax-1.1.3.js"></script>
-		<script src="assets/js/scripts.js"></script>
-	</body>
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jssor.slider.mini.js"></script>
+	<script src="assets/js/classie.js"></script>
+	<script src="assets/js/selectFx.js"></script>
+	<script src="assets/js/bootstrap-datepicker.min.js"></script>
+	<script src="assets/js/starrr.min.js"></script>
+	<script src="assets/js/nivo-lightbox.min.js"></script>
+	<script src="assets/js/jquery.shuffle.min.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="assets/js/gmaps.min.js"></script>
+	<script src="assets/js/jquery.parallax-1.1.3.js"></script>
+	<script src="assets/js/scripts.js"></script>
+</body>
+
 </html>
