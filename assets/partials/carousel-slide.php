2ed59c7e733908c125839875fd63cb873a875b96
Luis Alvarado <luis.alvaradofcb@outlook.es>
2019-05-25 10:43:28 -0400

    Se agrega img por encima del video

--- assets/partials/carousel-slide.php
+++ assets/partials/carousel-slide.php
@@ -1,9 +1,25 @@
 <div class="carousel slide" id="mega-slider" data-ride="carousel">
 				<div class="carousel-inner">
 				<div class="carousel-item active beactive">
+				<div style="position: relative;">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4"></div>
+						<div class="col-md-4">
+							<div style="position: absolute; margin-left: 65px;">
+								<img id="imglogo"src="assets/images/Logo_Casa_molle.png" alt="">
+							</div>
+							<div class="col-md-4"></div>
+						</div>
+					</div>
+				</div>
+					
+				</div>
 					<video width="100%" height="100%" autoplay playsinline loop muted style="border-top-style: solid;margin-bottom: -7px;">
   					<source src="assets/video/CASA MOLLE _ VILLA & GOLF (short).mp4" type="video/mp4">
 					</video>
 				</div>
 			</div>
 </div>
+
+
