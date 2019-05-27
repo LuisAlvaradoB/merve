0030ab167e131db7c9c26de21f2c99bf6779b332
Luis Alvarado <luis.alvaradofcb@outlook.es>
2019-05-27 15:47:25 -0400

    Add secciones suscribe

---
+++ assets/partials/banner-comunidad-section.php
@@ -0,0 +1,12 @@
+<section class="mg-book-now">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<h2 id="text-banner-suscribe">Suscríbete a comunidad CasaMolle</h2>
+			</div>
+			<div class="col-md-4">
+				<a href="https://casamolle.paxer.com/reservacion" target="_blank" class="btn btn-main btn-block">Suscribirse</a>
+			</div>
+		</div>
+	</div>
+</section>
--- index.php
+++ index.php
@@ -100,7 +100,20 @@
 	</header>
 
 	<?php require 'assets/partials/carousel-slide.php' ?>
-	<?php require 'assets/partials/reserva-section.php' ?>
+	<?php require 'assets/partials/banner-comunidad-section.php' ?>
+
+	<div class="mg-page-title main-img-firma ">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h4 id="text-main-firma">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, vero sint? Deserunt illo ea vitae quidem dolorem ducimus a quis ex ipsum animi. Iusto eum sequi minus id a optio?
+					</h4>
+					<img id="img-firma" src="assets/images/firma-text.png" alt="">
+					<h5 id="name-firma">Sir Lorem Ipsum</h5>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<section id="main-section-experiencias" class="mg-best-rooms">
 		<div class="container">
