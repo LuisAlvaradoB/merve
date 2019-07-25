6dc31f589b9d552fa8b9f744bbfcf4d079fcf473
Luis Alvarado <luis.alvaradofcb@outlook.es>
2019-07-25 10:17:29 -0400

    ADD form mailchimp

--- assets/partials/banner-comunidad-section.php
+++ assets/partials/banner-comunidad-section.php
@@ -1,14 +1,20 @@
-
 <section class="mg-book-now" style="padding: 20px 0px 15px 0px;">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-2"></div>
 			<div class="col-md-6 align-items-center">
-				<h2 id="text-banner-suscribe" class = "estilobanner">Comunidad CasaMolle</h2>
+				<h2 id="text-banner-suscribe" class="estilobanner">Comunidad CasaMolle</h2>
 			</div>
 			<div class="col-md-4">
-				<a href="https://casamolle.paxer.com/reservacion" target="_blank" class="btn btn-suscribe">Acompañanos</a>
+				<!-- <a href="https://casamolle.paxer.com/reservacion" target="_blank" class="btn btn-suscribe">Acompañanos</a> -->
 
+				<!-- Button trigger modal -->
+				<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">
+				Acompañanos
+				</button>
+
+				<!-- Modal -->
+				<?php require 'assets/partials/modal-mailchimp.php' ?>
 			</div>
 		</div>
 	</div>
